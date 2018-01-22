7a7c7bce487e08a6d04c750d44ab861acc7ce519
J Rose <josh@upros3.com>
2018-01-22 23:10:07 +0000

    Get contributors function finished

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,11 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader')
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  getRepoContributors("nodejs", "node", function(err, result) {
+    console.log("There was an error:", err);
+    console.log("Result:", result);
+  });
+}
+
