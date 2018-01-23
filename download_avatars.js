68770c8df745a8f88f8182455fc3867d30a9e281
J Rose <josh@upros3.com>
2018-01-23 00:11:31 +0000

    Added security for access token

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,25 @@
 var request = require('request');
+var secret = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader')
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  getRepoContributors("nodejs", "node", function(err, result) {
-    console.log("There was an error:", err);
-    console.log("Result:", result);
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+      'Authorization': secret.GITHUB_TOKEN;
+    }
+  };
+
+  request(url, function(err, res, body) {
+    cb(err, body);
   });
 }
+
+getRepoContributors("nodejs", "node", function(err, result) {
+  console.log("There was an error:", err);
+  console.log("Result:", result);
+});
+
 
