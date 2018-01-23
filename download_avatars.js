2007044c6a6053ab9ba290ce1a6cde1d5917e739
J Rose <josh@upros3.com>
2018-01-23 03:55:39 +0000

    Added comments and ensured final code is ready and working.

--- download_avatars.js
+++ download_avatars.js
@@ -3,39 +3,46 @@ var request = require('request');
 var secret = require('./secrets');
 var fs = require('fs');
 
-console.log('Welcome to the GitHub Avatar Downloader')
+console.log('Welcome to the GitHub Avatar Downloader');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': secret.GITHUB_TOKEN
-    }
-  };
+//Ensures that there are enough arguemnets for the application to work
+if (args.length === 2) {
+// Connects up to the github API
+  function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+      url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': secret.GITHUB_TOKEN
+      }
+    };
 
-  request(options, function(err, res, body) {
-    cb(err, body)
-  });
-}
-
-getRepoContributors(args[0], args[1], function(err, result, callback) {
-  console.log('Error:', err);
-  console.log('Downloading images...');
-  var js0n = JSON.parse(result);
-    for (var obj of js0n) {
-      downloadImageByURL(obj['avatar_url'], './' + './avatars/' + obj['login'] + '.jpg', obj['login']);
-    }
-
-  function downloadImageByURL(url, filePath, login) {
-    request.get(url)
-      .on('error', function (err) {
-      console.log('There was an error: ', err);
-    })
-      .on('response', function (response) {
-
-      console.log('Image of: ' + login + ' downloaded!');
-    })
-      .pipe(fs.createWriteStream(filePath));
+    request(options, function(err, res, body) {
+      cb(err, body)
+    });
   }
-});
+
+// Finds the URL's and usernames
+  getRepoContributors(args[0], args[1], function(err, result, callback) {
+    console.log('Error:', err);
+    console.log('Downloading images...');
+    var js0n = JSON.parse(result);
+      for (var obj of js0n) {
+        downloadImageByURL(obj['avatar_url'], './' + './avatars/' + obj['login'] + '.jpg', obj['login']);
+      }
+// Downloads the images
+    function downloadImageByURL(url, filePath, login) {
+      request.get(url)
+        .on('error', function (err) {
+        console.log('There was an error: ', err);
+      })
+        .on('response', function (response) {
+
+        console.log('Image of: ' + login + ' downloaded!');
+      })
+        .pipe(fs.createWriteStream(filePath));
+    }
+  });
+} else {
+    console.log('GitHub Avatar Downloader has not recieved 2 arguements. Please ensure that you have typed both [Username] [Repository].');
+}
